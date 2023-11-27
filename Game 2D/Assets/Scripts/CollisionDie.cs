a3fdff11aad63f4e7ca94580f8d10b696728020d
DAMIANKOBY <damiankoby@gmail.com>
2023-11-27 23:32:41 +0100

    poprawiona śmierć

--- Game 2D/Assets/Scripts/CollisionDie.cs
+++ Game 2D/Assets/Scripts/CollisionDie.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class CollisionDie : MonoBehaviour
 {
     public AudioClip DeadSound;
+    bool isColliding = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !isColliding)
         {
+            isColliding = true;
             collision.gameObject.GetComponent<Animator>().SetTrigger("Fail");
             AudioSource.PlayClipAtPoint(DeadSound, transform.position);
+
+            StartCoroutine(ResetCollisionFlag());
         }
     }
 
-    void Start()
+    IEnumerator ResetCollisionFlag()
     {
-        
-    }
+        // Wait for a short duration before resetting the flag
+        yield return new WaitForSeconds(2f); // Adjust the duration as needed
 
- 
-    void Update()
-    {
-
+        isColliding = false;
     }
 }
